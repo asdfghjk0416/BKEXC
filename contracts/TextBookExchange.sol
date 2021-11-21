a20e1a57b959508f9fc227b5ea269c349b367b30
asdfghjk0416 <57574033+asdfghjk0416@users.noreply.github.com>
2021-11-21 14:49:00 -0500

    added modifier

--- contracts/TextBookExchange.sol
+++ contracts/TextBookExchange.sol
@@ -1,40 +1,50 @@
 
-pragma solidity >=0.4.22;
-
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract TBE is ERC20 {
     
-    mapping(address => uint) public uploadCount;
+    mapping(address=>uint) public uploadCount;
     mapping(address=>uint) public rewardCount;
+    address payable SuperUser; //owner 
+    
+    modifier onlySuperUser()
+   { require(msg.sender == SuperUser); 
+      _;
+   }
+   
 
   constructor(uint _supply) ERC20("TextBookExchange","TEXEX"){
     _mint(msg.sender, _supply *(10**decimals())); 
     uploadCount[msg.sender] = 0;
     rewardCount[msg.sender] = 10;
   }
-  function UploadBook() public returns (uint256){
+   function UploadBook() public returns (uint256){
        _mint(msg.sender, 1*(10**decimals()));
        uploadCount[msg.sender] += 1;
        if(rewardCount[msg.sender] == uploadCount[msg.sender]) reward();
        return balanceOf(msg.sender);
-  }
+   }
    
-  function BuyBook() public returns (uint256){
+   function BuyBook() public returns (uint256){
        uint newVal = balanceOf(msg.sender)-(1*(10**decimals()));
        require(newVal >= 0);
        _burn(msg.sender, 1*(10**decimals()));
        return balanceOf(msg.sender);
-  }
+   }
    
-  function uploadCountofUser() public view returns (uint){
+   function uploadCountofUser() public view returns (uint){
        return uploadCount[msg.sender];
-  }
+   }
    
-  function reward() internal returns(bool success) {
+   function reward() internal returns(bool success) {
         _mint(msg.sender,1*(10**decimals()));
         rewardCount[msg.sender] *= 2;
         return true;
-  }
+   }
+   
+   function removeSpamBooks(address person) public onlySuperUser {
+       _burn(person, 10*(10**decimals()));
+       uploadCount[person] -= 1;
+   }
 }
