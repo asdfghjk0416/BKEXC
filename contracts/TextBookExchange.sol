2256f5b8f94c4dd1f7bf820cc7d0d4de77eaa51c
asdfghjk0416 <57574033+asdfghjk0416@users.noreply.github.com>
2021-12-04 13:33:35 -0500

    new folder structure without react

--- contracts/TextBookExchange.sol
+++ contracts/TextBookExchange.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22;
+
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
@@ -6,45 +9,64 @@ contract TBE is ERC20 {
     
     mapping(address=>uint) public uploadCount;
     mapping(address=>uint) public rewardCount;
-    address payable SuperUser; //owner 
+    mapping(address=>bool) public isBanned;
+    mapping (address=>uint) public SpamCount;
+    address SuperUser; //owner 
     
     modifier onlySuperUser()
-   { require(msg.sender == SuperUser); 
+  { 
+      require(msg.sender==SuperUser);
       _;
+      
+  }
+
+   
+   modifier ValidUser()
+   { require(isBanned[msg.sender]==false);
+       _;
    }
    
 
+    
   constructor(uint _supply) ERC20("TextBookExchange","TEXEX"){
-    _mint(msg.sender, _supply *(10**decimals())); 
+    _mint(msg.sender, _supply*(10**decimals()));
+    SuperUser = msg.sender;
     uploadCount[msg.sender] = 0;
     rewardCount[msg.sender] = 10;
   }
-   function UploadBook() public returns (uint256){
+   function UploadBook() public ValidUser returns (uint256){
        _mint(msg.sender, 1*(10**decimals()));
        uploadCount[msg.sender] += 1;
        if(rewardCount[msg.sender] == uploadCount[msg.sender]) reward();
        return balanceOf(msg.sender);
    }
    
-   function BuyBook() public returns (uint256){
+   function BuyBook() public ValidUser returns (uint256){
        uint newVal = balanceOf(msg.sender)-(1*(10**decimals()));
        require(newVal >= 0);
        _burn(msg.sender, 1*(10**decimals()));
        return balanceOf(msg.sender);
    }
    
-   function uploadCountofUser() public view returns (uint){
+   function uploadCountofUser() public view ValidUser returns (uint){
        return uploadCount[msg.sender];
    }
    
-   function reward() internal returns(bool success) {
+   function reward() internal ValidUser returns(bool) {
         _mint(msg.sender,1*(10**decimals()));
         rewardCount[msg.sender] *= 2;
         return true;
    }
    
-   function removeSpamBooks(address person) public onlySuperUser {
-       _burn(person, 10*(10**decimals()));
+   function removeSpamBooks(address person) payable public onlySuperUser returns(bool) {
+       require(uploadCount[person]>0);
+       uint newBal = balanceOf(person) / 2;
+       _burn(person, newBal);
        uploadCount[person] -= 1;
+       SpamCount[person] += 1;
+       if (SpamCount[person]>= 2){
+           isBanned[person] = true;
+       }
+       return true;
    }
 }
