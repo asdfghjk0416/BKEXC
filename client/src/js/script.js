ccb9d53225c81888ca99884a4706a3b6c1845cd2
Swati Suresh <swatisur@buffalo.edu>
2021-12-05 08:56:21 -0500

    Completed HTML and its functionality

---
+++ client/src/js/script.js
@@ -0,0 +1,27 @@
+function openUploadBook(){
+    if (document.getElementById("fm").style.display=="none"){
+   document.getElementById("fm").style.display = "";
+    }
+    else{
+       document.getElementById("fm").style.display = "none";
+    }
+  }
+
+  function openBuyBook(){
+  if (document.getElementById("fm1").style.display=="none"){
+   document.getElementById("fm1").style.display = "";
+    }
+    else{
+       document.getElementById("fm1").style.display = "none";
+    }
+  }
+  
+  function openMarkSpam(){
+    if (document.getElementById("fm2").style.display=="none"){
+   document.getElementById("fm2").style.display = "";
+    }
+    else{
+       document.getElementById("fm2").style.display = "none";
+    }
+  }
+  
